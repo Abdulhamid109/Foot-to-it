1f328fb979d24ae570d69ec2142c05792277b64b
Abdulhamid Patel <2000147400@hexaware.com>
2025-10-02 23:31:52 +0530

    converted the button to link (testing purpose)

--- app/user/auth/login/page.tsx
+++ app/user/auth/login/page.tsx
@@ -39,7 +39,7 @@ const LoginPage = () => {
             />
           </div>
           <Button
-            type="submit"
+            //type="submit"
             className="w-full bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-700 text-white py-2 rounded-lg font-medium transition-all"
           >
             <Link href={'/user/pages/home'}>Login</Link>
